c38afcd0ccf4de36f7c798b372451ed8e6fddcb0
rvanderlely <101234963+rvanderlely@users.noreply.github.com>
2023-03-07 14:56:12 -0600

    added comment

--- creds.php
+++ creds.php
@@ -2,6 +2,11 @@
     $host = 'localhost';
     $dbname = 'team1';
     $user = 'root';
-    $pass = '';
+    $pass = 'root';
     $port = '3306';
+//need to change password to root after each pulldown for mac
+//for windows the password needs to be deleted. 
 ?>
+
+
+
