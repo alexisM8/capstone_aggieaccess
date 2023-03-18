8a3b18091c0257108a02049d8dbe7b76e89b214a
Avontae Broomfield <avontaebroomfield@Avontaes-Air.attlocal.net>
2023-03-17 21:41:21 -0500

    more comments added from tutorial

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
+    <title>Form</title>
 </head>
 <body>
     <form action = "test.php" method = "post"><!--form is a special html element that allows the user to input info. 
@@ -15,18 +15,154 @@ that test.php handles what happens with the form. The method tells the form what
 <!--Oranges:<input type= "checkbox" name ="fruits[]" value ="oranges" > <br> -->
 <!--Pears:<input type= "checkbox" name ="fruits[]" value ="pears" > <br>  -->
 <!--<input type="text" name="student"> -->
-<!--<input type="submit"> will submit all the info--> 
+<!--<input type="submit"> will create a submit button to submit all the info--> 
+    <!-- What was your grade?
+    <input type="text" name="grade">
+    <input type="submit"> -->
     </form>
+
+<?php 
+class Movie{
+    public $title; //keyword that tells  php what code is used to access this code in the program. public tells that this attribute is visible across the program. var does the same thing
+    private $rating; // this keyword tells that any code outside of this class isn't gonna access the this attribute directly    
+
+function __construct($title, $rating){
+    $this->title = $title;
+    $this->setRating($rating);
+}
+function getRating(){
+    return $this->rating;
+}
+
+function setRating($rating){ 
+    if ($rating == "G" || $rating == "PG") {
+        $this->rating = $rating;
+    }else{
+        $this->rating = "NR";
+    }
+}
+
+}
+
+$avengers = new Movie("Avengers", "PG-13");
+
+$avengers->setRating("G");
+
+echo $avengers->getRating();
+?>
+
+    <?php 
+    // class Student{
+    //     var $name;
+    //     var $major;
+    //     var $gpa;
+
+    // function __construct($name, $major, $gpa)
+    // {
+    //     $this->name = $name;
+    //     $this->major = $major;
+    //     $this->gpa = $gpa;
+    // }
+    // function hasHonors(){
+    //     if ($this->gpa >= 3.5) {
+    //         return "true";
+    //     }
+    //     return "false";
+    // }
+
+    // }
+    // $student1 = new student("Jim", "Business", 2.8);
+    // $student2 = new student("Pam", "Art", 3.6);
+    // echo $student2->hasHonors();
+    ?>
+    <?php
+    // class Book{ //creating a Book class. 
+    //     var $title; // attributes of the Book class. 
+    //     var $author;
+    //     var $pages;
+
+    //     function __construct($aTitle, $aAuthor, $aPages){// a special function inside a class, which will be called once we create an object of that class
+    //         $this->title = $aTitle; //the this keyword refers to the current object thats being created. this title is = to the title that got passed in
+    //         $this->author = $aAuthor;
+    //         $this->pages = $aPages;
+    //     }
+    // }
+    // $book1 = new Book("Harry Potter", "JK Rowling", 400);// create a new book data type. This is an object, an object is an instance of a class. new Book calls the constructor function
+    // $book1->title = "Harry Potter";// assigning the attributes in the Book class
+    // $book1->author = "JK Rowling";
+    // $book1->pages = 400;
+    // echo $book1->author;// printing the attributes
+
+    // $book2 = new Book("Lord of the Rings", "Tolkien", 700);// create a new book data type. This is an object, an object is an instance of a class. new Book calls the constructor function
+    // $book2->title = "Lord of the Rings";// assigning the attributes in the Book class
+    // $book2->author = "Tolkien";
+    // $book2->pages = 700;
+    // echo $book2->author;// printing the attributes
+    // echo $book1->title;
+    ?>
     
     <?php
-    
+    //include "header.html" //includes a separate file into the php 
+    // $index = 1;
+    // do{
+    //     echo "$index <br>";
+    //     $index++;
+    // }while($index <= 5);
+    // $luckyNumbers = array(4,8,14,16,23,42);
+    // for($i = 0; $i < count($luckyNumbers); $i++){
+    //     echo "$luckyNumbers[$i] <br>";
+    // }
+    // $grade = $_POST["grade"];
+    // switch($grade){
+    //     case "A":
+    //         echo "You did amazing";
+    //         break;
+    //     case "B":
+    //         echo "You did pretty good";
+    //         break;
+    //     case "C":
+    //         echo "You did alright";
+    //         break;
+    //     case "D":
+    //         echo "You did poorly";
+    //         break;
+    //     case "F":
+    //         echo "YOU FAIL";
+    //         break;
+    //     default:
+    //     echo "Invalid Grade";
+    // }
+    // function getMax($num1, $num2, $num3){
+    //     if($num1 >= $num2 && $num1 >= $num3){
+    //         return $num1;
+    //     }else if($num2 >= $num1 && $num2 >= $num3){
+    //             return $num2;
+    //         }else {
+    //             return $num3;
+    //         }
+    // }
+    //     echo getMax(3000,900,3000);
+    // $isTall = true;
+    // $isMale = false;
+    // if($isMale || $isTall){
+    //     echo "You are a tall Male";
+    // }else if($isMale && !$isTall){
+    //     echo "You are a short Male";
+    // }else if(!$isMale && $isTall){
+    //     echo "You are not Male but are tall";
+    // }
+    // else{
+    //     echo "You are not a tall Male and not Tall";
+    // }
+
     /*
     function cube($num){
-        //echo "Hello";// will print this out since it came before return
+        echo "Hello";// will print this out since it came before return
         return $num * $num * $num; // the return keyword RETURNS the value back to the caller, 
-        //echo "Hello"; //php will break out of the function once it comes across return, will never print Hello
+        echo "Hello"; //php will break out of the function once it comes across return, will never print Hello
     }*/
     //cube(4); // will give us the four cubed which is 64.
+
     /*function sayHi($name){ // small program to do a certain task
         echo "Hello $name";
     }
@@ -75,6 +211,7 @@ that test.php handles what happens with the form. The method tells the form what
     // echo "He really liked the name $name <br> ";
     // echo "But he didn't like being $age <br> ";
     ?>
+
 
 </body>
 </html>
