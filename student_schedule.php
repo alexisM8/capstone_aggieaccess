fa1d5658f15893c6335aa399453ea59e8815b8ea
Kettisark Dy <kettisarkkd@gmail.com>
2023-03-22 17:55:59 -0500

    Created a basic template using HTML and CSS of a student home page and student view schedule page. Also separated CSS into external stylesheets.

---
+++ student_schedule.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+    <html>
+    <style>
+    ul{
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        float: left;
+
+    }
+
+    table, th, td {
+    border:1px solid black;
+    }
+
+    button{
+        float: right;
+    }
+    </style>
+    <head>
+
+    </head>
+    <body>
+        <h1> View Schedule</h1>
+        <div>
+            <ul>
+                <li>Name: </li>
+                <li>Classification: </li>
+                <li>Major: </li>
+            </ul>
+        </div>
+
+        <table style="width:100%">
+    <tr>
+        <th>CRN</th>
+        <th>Course</th>
+        <th>Title</th>
+        <th>Time</th>
+        <th>Days</th>
+        <th>Location</th>
+        <th>Instructor</th>
+    </tr>
+    <tr>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td><button>Remove</button></td>
+    </tr>
+    <tr>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td><button>Remove</button></td>
+    </tr>
+    <tr>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td>Test</td>
+        <td><button>Remove</button></td>
+    </tr>
+    </table>
+
+        <button>Print Schedule</button>
+    </body>
+    </html>
