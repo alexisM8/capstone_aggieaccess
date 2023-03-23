37d9bbc68be6647162fafc0c27a9bc274eb8f5a0
Abdul Rahman <account@Abdul.local>
2023-03-23 15:05:25 -0500

    added secretary folders/files and deleted unncessary html files

---
+++ Secretary/Home.php
@@ -0,0 +1,59 @@
+
+<?php
+    require_once 'creds.php';
+
+    $conn = new mysqli($host, $user, $pass, $dbname, $port);
+    
+    if($conn->connect_error){
+        die("Fatal Error");
+    }
+    session_start();
+    $student_id = $_SESSION['id'];
+    $student_user = $_SESSION['user_type'];
+    $login_check=$_SESSION['loggedin'];
+    if((isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === true) &&($_SESSION['user_type']==='faculty'))
+  {
+// Define the SQL query
+$sql = "SELECT f.email ,f.fname,f.lname,f.phone
+        FROM  faculty AS f
+        WHERE f.fid = '$student_id'";
+    $result = mysqli_query($conn, $sql);
+
+// Check if the query was successful
+if (!$result) {
+  die("Query failed: " . mysqli_error($conn));
+}
+
+// Process the result
+if (mysqli_num_rows($result) > 0) {
+    
+    echo "<table>";
+    echo "<tr><th>First Name</th><th>Last Name</th></tr>";
+    
+    // Output data of each row
+    $row = mysqli_fetch_assoc($result);
+    echo "<tr><td>" . $row["fname"] . "</td><td>" . $row["lname"] . "</td></tr>";
+    echo '</br>';
+    echo '</br>';
+    echo '</br>';
+    echo '</br>';
+    echo '</br>';
+    echo '</br>';
+    echo "</table>";
+    echo "<table>";
+    echo "<tr><th>Secretary Email</th><th>Phone Number</th></tr>";
+    echo "<tr><td>" . $row["email"] . "</td><td>" . $row["phone"] . "</td></tr>";
+    echo "</table>";
+
+    
+  } else {
+    echo "<table>";
+    echo "<tr><th>No results found</th></tr>";
+    echo "</table>";
+  }
+}
+else
+{
+  header("Location: login.php");
+}
+?>
---
+++ Secretary/ListItStudent.php
@@ -0,0 +1,58 @@
+
+<?php
+    require_once 'creds.php';
+
+    $conn = new mysqli($host, $user, $pass, $dbname, $port);
+    
+    if($conn->connect_error){
+        die("Fatal Error");
+    }
+    session_start();
+    if((isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === true) &&($_SESSION['user_type']==='faculty'))
+    {
+    // Replace the student email with the desired student's email
+
+// Prepare the SQL statement
+$sql = "SELECT student.fname, student.lname, student.email,student.major 
+        FROM   student
+        WHERE student.major = 'IT'";
+
+// Execute the SQL statement and get the result
+$result = mysqli_query($conn, $sql);
+
+// Check if there are any results
+if ($result) {
+if (mysqli_num_rows($result) >0) {
+
+    // Create a table to display the results
+    echo "<h1 style='text-align: center; color: black;'>"."IT Student List"."</h1>";
+    echo "<table>";
+    echo "<tr><th>Student Name</th><th>Student Email</th><th>Student Department</th></tr>";
+
+    // Loop through the results and display them in the table
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["fname"] . " " . $row["lname"] . "</td><td>" . $row["email"] . "</td><td>" . $row["major"] . "</td></tr>";
+    }
+
+    echo "</table>";
+
+} else {
+    echo "<table>";
+    echo "<tr><th>No Student Found in IT department.</th></tr>";
+    
+    echo "</table>";
+}
+}
+else {
+    // display error message if query execution failed
+    echo 'Error executing query: ' . mysqli_error($conn);
+}
+
+// Close the database connection
+$conn->close();
+}
+else
+{
+header("Location: login.php");
+}
+?>
