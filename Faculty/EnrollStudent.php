ce538509f524f8633e90555baf6e6cc260b9da37
Nettles <aaron.i.nettles@gmail.com>
2023-03-28 20:46:29 -0500

    enroll page changes

--- Faculty/EnrollStudent.php
+++ Faculty/EnrollStudent.php
@@ -5,11 +5,30 @@
     
     if($conn->connect_error){
         die("Fatal Error");
-    }?>
+    }
+    if (isset($_POST['submit'])) {
+        $department = $_POST['department'];
+    
+        // Query to select all classes in a department
+        $sql = "SELECT course.courseTitle, course.CRN, faculty.lname, time.timeRange, date.startDate, date.endDate, rooms.roomNum, building.buildAbbrv
+                FROM course 
+                JOIN class ON course.courseID = class.courseID 
+                JOIN time ON class.timeID = time.timeID 
+                JOIN date ON class.dateID = date.dateID 
+                JOIN location ON class.locationID = location.locationID 
+                JOIN rooms ON location.roomID = rooms.roomID
+                JOIN building ON location.buildID = building.buildID
+                JOIN faculty ON class.profID = faculty.fid
+                JOIN department ON department.departmentID = course.departmentID
+                WHERE department.departmentAbbrv = '$department'";
+    
+        $result = $conn->query($sql);
+    }
+    ?>
 <!DOCTYPE html>
 <html>
 
-<form method="post" action="EnrollStudent.php">
+<form method="post" action="?page=EnrollStudent">
     <label>Choose Student Email</label>
     <?php
     $sql2 = "SELECT * FROM student";
@@ -38,6 +57,19 @@
 
     <br><br>
 
-    <input type="submit" value="Submit">
+    <input name = "submit" type="submit" value="Submit">
 </form>
+<?php
+// Display table of classes in the selected department
+if (isset($result) && $result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr><th>Course Title</th><th>CRN</th><th>Professor</th><th>Time</th><th>Start Date</th><th>End Date</th><th>Room Number</th><th>Building</th></tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["courseTitle"] . "</td><td>" . $row["CRN"] . "</td><td>" . $row["lname"] . "</td><td>" . $row["timeRange"] . "</td><td>" . $row["startDate"] . "</td><td>" . $row["endDate"] . "</td><td>" . $row["roomNum"] . "</td><td>" . $row["buildAbbrv"] . "</td></tr>";
+    }
+
+    echo "</table>";
+}
+?>
 </html>
