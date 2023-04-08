78e6e18132518f0e66e16e32bd355b00f3db696a
Avontae Broomfield <avontaebroomfield@Avontaes-Air.attlocal.net>
2023-04-08 10:13:17 -0500

    fixed showing the chairs first & last name in chair/ListTeachingSchedule

--- chair/ListTeachingSchedule.php
+++ chair/ListTeachingSchedule.php
@@ -8,29 +8,39 @@ if ($conn->connect_error) {
     die("Fatal Error");
 }
 session_start();
+$f_id=$_SESSION['id'];
+$student_id = $_SESSION['id'];
+$student_user = $_SESSION['user_type'];
+$login_check=$_SESSION['loggedin'];
 
+$sql = "SELECT f.email ,f.fname,f.lname,f.phone
+        FROM  faculty AS f
+        WHERE f.fid = '$student_id'";
 
-        $sql = "SELECT f.email ,f.fname,f.lname,f.phone
-        FROM  faculty AS f";
+$result = mysqli_query($conn, $sql);
 
-    $result = $conn->query($sql);
+echo "<b>Your Information:</b>";
+echo "<table>";
+echo "<tr><th>First Name</th><th>Last Name</th></tr>";
 
-    $row = mysqli_fetch_assoc($result);
-    echo "<b>Your Information:</b>";
-    echo "<table>";
-    echo "<tr><th>First Name</th><th>Last Name</th></tr>";
-    
-    echo "<tr><td>" . $row["fname"] . "</td><td>" . $row["lname"] . "</td></tr>";
-    echo "</table>";
+$row = mysqli_fetch_assoc($result);
+echo "<tr><td>" . $row["fname"] . "</td><td>" . $row["lname"] . "</td></tr>";
+echo "</table>";
 
-    echo "<table>";
-    echo "<tr><th>Faculty Email</th><th>Phone Number</th></tr>";
-    $row = mysqli_fetch_assoc($result);
-    echo "<tr><td>" . $row["email"] . "</td><td>" . $row["phone"] . "</td></tr>";
-    echo "</table>";
+echo "<table>";
+echo "<tr><th>Faculty Email</th><th>Phone Number</th></tr>";
+echo "<tr><td>" . $row["email"] . "</td><td>" . $row["phone"] . "</td></tr>";
+echo "</table>";
 
-    if((isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === true) &&($_SESSION['user_type']==='faculty'))
-  {
+    if((isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === true) &&($_SESSION['user_type']==='faculty')){
+    echo '
+    <form action="?page=ListTeachingSchedule" method="POST">
+    <label for="username">Enter Teacher Last name:</label>
+    <input type="text" id="username" name="username">
+    <input type="submit" name="submit" value="Find Teacher Schedule">
+    </form>';
+    if(isset($_POST['submit'])){
+    $lastname = $_POST['username'];
 $sql_select = "SELECT DISTINCT c.courseTitle AS Course_Title,
 f.fname AS FirstName,   
 f.lname AS LastName,      
@@ -41,7 +51,7 @@ dt.endDate AS End_Date,
 r.roomNum AS Room 
 FROM course AS c INNER JOIN class AS cl ON c.courseID = cl.courseID 
 INNER JOIN enrollment AS e ON cl.classID = e.classID 
-INNER JOIN faculty AS g ON e.facultyID = g.fid
+INNER JOIN faculty AS g ON e.facultyID = g.fid AND g.lname = '$lastname'
 INNER JOIN enrollment AS z ON cl.classID = z.classID AND z.facultyID = g.fid
 INNER JOIN faculty AS f ON z.facultyID = f.fid 
 INNER JOIN time AS t ON cl.timeID = t.timeID 
@@ -54,8 +64,8 @@ $result = $conn->query($sql_select);
 // Check for errors in the SELECT query
 if (!$result) {
     echo "Error selecting record: " . $conn->error;
-} if (mysqli_num_rows($result) >0) {
-    
+} 
+if (mysqli_num_rows($result) > 0) {    
     echo "<table>";
     echo "<tr><th>Faculty Name</th><th>Course Tilte</th><th>Time</th><th>Start Date</th><th>end Date</th><th>room Num</th></tr>";
     while($row = $result->fetch_assoc()) {
@@ -68,11 +78,12 @@ if (!$result) {
 }
 else {
     echo "<table>";
-    echo "<tr><th>No Class Found</th></tr>";
+    echo "<tr><th>No Teacher Schedule</th></tr>";
     
     echo "</table>";
 }
 }
+  }
 else
 {
 header("Location: login.php");
