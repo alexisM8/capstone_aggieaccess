154210d1edd742351ccaa58a63ebb1cb2bd4aad2
Avontae Broomfield <avontaebroomfield@Avontaes-Air.attlocal.net>
2023-04-12 22:30:28 -0500

    Changed up the html and css of the grant override a little

--- chair/GrantOverride.php
+++ chair/GrantOverride.php
@@ -4,10 +4,15 @@ $conn = new mysqli($host, $user, $pass, $dbname, $port);
 if ($conn->connect_error) {
     die("Fatal Error");
 }
-if (isset($_COOKIE['newcount'])) {
-    $count = $_COOKIE['newcount'];
-}
-echo "<h2>"."Allow Seat up to    :" .$count."</h2>";
+// if (isset($_COOKIE['newcount'])) {
+//     $count = $_COOKIE['newcount'];
+// }
+            echo "<h2> Grant Override </h2> ";
+            echo "<hr>";
+            echo "<h3 style = color:red; ><strong> IMPORTANT NOTICE!!! </strong></h3>";
+            echo "<p><strong> In order for there to be a class there must be a minimum of 12 students per class,<br> 
+            with a maximum of 20 students per class. </strong> </p>";
+
 echo '<form method="POST" action="?page=GrantOverride">';
 echo '<input type="submit" name="submit" value="Grant Override">';
 echo '</form>';
