0115815e46f77ba75ae95e445fe4cda1fae388d4
Kettisark Dy <kettisarkkd@gmail.com>
2023-04-18 15:08:52 -0500

    Added building abbrv. to most schedule pages

--- Faculty/ListTeachingSchedule.php
+++ Faculty/ListTeachingSchedule.php
@@ -41,24 +41,26 @@ echo "</table>";
     </form>';
     if(isset($_POST['submit'])){
     $lastname = $_POST['username'];
-$sql_select = "SELECT DISTINCT c.courseTitle AS Course_Title,
-f.fname AS FirstName,   
-f.lname AS LastName,      
-t.timerange AS Time,        
-d.days AS Meeting_Days,        
-dt.startDate AS Start_Date,        
-dt.endDate AS End_Date,        
-r.roomNum AS Room 
-FROM course AS c INNER JOIN class AS cl ON c.courseID = cl.courseID 
-INNER JOIN enrollment AS e ON cl.classID = e.classID 
-INNER JOIN faculty AS g ON e.facultyID = g.fid AND g.lname = '$lastname'
-INNER JOIN enrollment AS z ON cl.classID = z.classID AND z.facultyID = g.fid
-INNER JOIN faculty AS f ON z.facultyID = f.fid 
-INNER JOIN time AS t ON cl.timeID = t.timeID 
-INNER JOIN day AS d ON cl.dayID = d.daysID 
-INNER JOIN date AS dt ON cl.dateID = dt.dateID 
-INNER JOIN location AS l ON cl.locationID = l.locationID 
-INNER JOIN rooms AS r ON l.roomID = r.roomID";
+    $sql_select = "SELECT DISTINCT c.courseTitle AS Course_Title,
+        f.fname AS FirstName,   
+        f.lname AS LastName,      
+        t.timerange AS Time,        
+        d.days AS Meeting_Days,        
+        dt.startDate AS Start_Date,        
+        dt.endDate AS End_Date,        
+        r.roomNum AS Room, 
+        b.buildAbbrv AS Build_Abbrv
+        FROM course AS c INNER JOIN class AS cl ON c.courseID = cl.courseID 
+        INNER JOIN enrollment AS e ON cl.classID = e.classID 
+        INNER JOIN faculty AS g ON e.facultyID = g.fid AND g.lname = '$lastname'
+        INNER JOIN enrollment AS z ON cl.classID = z.classID AND z.facultyID = g.fid
+        INNER JOIN faculty AS f ON z.facultyID = f.fid 
+        INNER JOIN time AS t ON cl.timeID = t.timeID 
+        INNER JOIN day AS d ON cl.dayID = d.daysID 
+        INNER JOIN date AS dt ON cl.dateID = dt.dateID 
+        INNER JOIN location AS l ON cl.locationID = l.locationID 
+        INNER JOIN building AS b ON l.buildID = b.buildID
+        INNER JOIN rooms AS r ON l.roomID = r.roomID";
 $result = $conn->query($sql_select);
 
 // Check for errors in the SELECT query
@@ -67,10 +69,10 @@ if (!$result) {
 } 
 if (mysqli_num_rows($result) > 0) {    
     echo "<table>";
-    echo "<tr><th>Faculty Name</th><th>Course Tilte</th><th>Time</th><th>Start Date</th><th>end Date</th><th>room Num</th></tr>";
+    echo "<tr><th>Faculty Name</th><th>Course Title</th><th>Time</th><th>Start Date</th><th>End Date</th><th>Room</th><th>Building Abbrv.</th></tr>";
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>" . $row["FirstName"] . " " . $row["LastName"] . "</td><td>" . $row["Course_Title"] . "</td><td>" . $row["Time"] . "</td><td>" . 
-        $row["Start_Date"] . "</td><td>" . $row["End_Date"] . "</td><td>" . $row["Room"] . "</td></tr>";
+        $row["Start_Date"] . "</td><td>" . $row["End_Date"] . "</td><td>" . $row["Room"] . "</td><td>" . $row["Build_Abbrv"] . "</td></tr>";
     }
 
     echo "</table>";
