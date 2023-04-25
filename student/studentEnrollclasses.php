3b64e6c3e53022374b68c6bba866cbec0033dc76
chirsa430 <cargyros430@gmail.com>
2023-04-25 16:32:56 -0500

    student enroll classes

--- student/studentEnrollclasses.php
+++ student/studentEnrollclasses.php
@@ -7,6 +7,19 @@ if ($conn->connect_error) {
     die("Fatal Error");
 }
 session_start();
+// Handle the enrollment form submission
+if (isset($_POST['enroll'])) {
+    $classID = $_POST['classID'];
+    $studentID = $_SESSION['id'];
+    $facultyID = $_POST['facultyID'];
+
+    $sql_enroll = "INSERT INTO enrollment (studentID, classID, facultyID) VALUES ('$studentID', '$classID', '$facultyID')";
+    if ($conn->query($sql_enroll) === TRUE) {
+        echo "Successfully enrolled in the class.";
+    } else {
+        echo "Error: " . $sql_enroll . "<br>" . $conn->error;
+    }
+}
 $student_user = $_SESSION['user_type'];
 $login_check = $_SESSION['loggedin'];
 if (($_SESSION['user_type'] === 'student')) {
@@ -64,7 +77,9 @@ if (isset($_SESSION['pin_verified']) && $_SESSION['pin_verified'] === true) {
         d.days AS Meeting_Days,        
         dt.startDate AS Start_Date,        
         dt.endDate AS End_Date,        
-        r.roomNum AS Room 
+        r.roomNum AS Room,
+        b.buildAbbrv AS Build_Abbrv,        
+        cl.seatLimit AS Seat_Limit 
             FROM course AS c INNER JOIN class AS cl ON c.courseID = cl.courseID  
             INNER JOIN faculty AS f ON cl.profID = f.fid 
             INNER JOIN time AS t ON cl.timeID = t.timeID 
@@ -72,6 +87,7 @@ if (isset($_SESSION['pin_verified']) && $_SESSION['pin_verified'] === true) {
             INNER JOIN date AS dt ON cl.dateID = dt.dateID 
             INNER JOIN location AS l ON cl.locationID = l.locationID 
             INNER JOIN rooms AS r ON l.roomID = r.roomID
+            INNER JOIN building AS b ON l.buildID = b.buildID
             INNER JOIN department on c.departmentID = department.departmentID
             WHERE department.departmentAbbrv = '$departmentAbbrv'";
                 
@@ -81,57 +97,79 @@ if (isset($_SESSION['pin_verified']) && $_SESSION['pin_verified'] === true) {
             if ($result) {
                 if (mysqli_num_rows($result) > 0) {
                     // Create a table to display the results
-                    echo "<table>";
+echo "<table>";
+echo "<tr>
+<th>Action</th>
+<th>Course Name</th>
+<th>Course Instructor</th>
+<th>Course Time</th>
+<th>Meeting Days</th>
+<th>Session Start</th>
+<th>Session End</th>
+<th>Room Number</th>
+<th>Building Abbrv.</th>
+<th>Seat Limit</th>
+</tr>";
+// Loop through the results and display them in the table
+while($row = $result->fetch_assoc()) {
+$classID = $row['CLID'];
+$studentID = $_SESSION['id'];
+$sql_check_enrollment = "SELECT * FROM enrollment WHERE classID = '$classID' AND studentID = '$studentID'";
+$result_check_enrollment = mysqli_query($conn, $sql_check_enrollment);
+$alreadyEnrolled = mysqli_num_rows($result_check_enrollment) > 0;
+$sql_check_students_enrolled = "SELECT count(enrollment.studentID) as numOfStudentsEnrolled 
+                                                    from enrollment where classID = '$classID'";
+                    $result_check_students_enrolled = ($conn->query($sql_check_students_enrolled))->fetch_assoc();
+                    $studentsEnrolled = $result_check_students_enrolled['numOfStudentsEnrolled'];
+                    $seatLimit = $row['Seat_Limit'];
+                    $seatsAvailable = $studentsEnrolled < $seatLimit;
+
                     echo "<tr>
-                            <th>Action</th>
-                            <th>Course Name</th>
-                            <th>Course Instructor</th>
-                            <th>Course Time</th>
-                            <th>Meeting Days</th>
-                            <th>Session Start</th>
-                            <th>Session End</th>
-                            <th>Room Number</th>
-                        </tr>";
-                    // Loop through the results and display them in the table
-                    while($row = $result->fetch_assoc()) {
-                        echo "<tr>
-                                <td>
-                                    <form method='POST'>
-                                        <input type='hidden' name='classID' value='" . $row['CLID'] . "'>
-                                        <input type='hidden' name='facultyID' value='". $row['FID'] . "'>
-                                        <input type='hidden' name='className' value='". $row['Course_Title'] . "'>
-                                        <button type='submit' name='enroll' value='enroll'>Enroll</button>
-                                    </form>
-                                </td>
-                                <td>" . $row["Course_Title"] . "</td>
-                                <td>"  . $row["Instructor"] ."</td>
-                                <td>"  . $row["Time"] ."</td>
-                                <td>"  . $row["Meeting_Days"] ."</td>
-                                <td>"  . $row["Start_Date"] ."</td>
-                                <td>"  . $row["End_Date"] ."</td>
-                                <td>"  . $row["Room"] ."</td>
-                            </tr>";
+                            <td>";
+                    if (!$alreadyEnrolled && $seatsAvailable) {
+                        echo "<form method='POST'>
+                                    <input type='hidden' name='classID' value='" . $row['CLID'] . "'>
+                                    <input type='hidden' name='facultyID' value='". $row['FID'] . "'>
+                                    <input type='hidden' name='className' value='". $row['Course_Title'] . "'>
+                                    <button type='submit' name='enroll' value='enroll'>Enroll</button>
+                                </form>";
+                    } elseif ($alreadyEnrolled) {
+                        echo "Already Enrolled";
+                    } else {
+                        echo "Class Full";
                     }
-                    
-                    echo "</table>";
-                } else {
-                    echo "No classes found for specified department";
+                    echo "</td>
+                            <td>" . $row["Course_Title"] . "</td>
+                            <td>"  . $row["Instructor"] ."</td>
+                            <td>"  . $row["Time"] ."</td>
+                            <td>"  . $row["Meeting_Days"] ."</td>
+                            <td>"  . $row["Start_Date"] ."</td>
+                            <td>"  . $row["End_Date"] ."</td>
+                            <td>"  . $row["Room"] ."</td>
+                            <td>"  . $row["Build_Abbrv"] ."</td>
+                            <td>"  . $row["Seat_Limit"] ."</td>
+                        </tr>";
                 }
+                
+                echo "</table>";
             } else {
-                // display error message if query execution failed
-                echo 'Error executing query: ' . mysqli_error($conn);
+                echo "No classes found for specified department";
             }
-    }
-    ?>
-    </body>
-    </html>
-    <?php
-    } else {
-        // Display the form to input the PIN
-        echo '<form action="" method="POST">';
-        echo '<label for="pin">Enter PIN:</label>';
-        echo '<input type="text" id="pin" name="pin" required>';
-        echo '<input type="submit" name="submit_pin" value="Submit PIN">';
-        echo '</form>';
-    }
-    ?>
+        } else {
+            // display error message if query execution failed
+            echo 'Error executing query: ' . mysqli_error($conn);
+        }
+}
+?>
+</body>
+</html>
+<?php
+} else {
+    // Display the form to input the PIN
+    echo '<form action="" method="POST">';
+    echo '<label for="pin">Enter PIN:</label>';
+    echo '<input type="text" id="pin" name="pin" required>';
+    echo '<input type="submit" name="submit_pin" value="Submit PIN">';
+    echo '</form>';
+}
+?>
