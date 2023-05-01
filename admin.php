710dc06ce071713948fef4bcd60931602b6f84d4
Avontae Broomfield <avontaebroomfield@Avontaes-MacBook-Air.local>
2023-05-01 09:42:59 -0500

    changed the background color for student,admin,faculty pages

--- admin.php
+++ admin.php
@@ -17,6 +17,7 @@
 			padding: 0;
 
 			font-family: Arial, sans-serif;
+			background-color: #f2f2f2;
 
 		}
 
--- faculty.php
+++ faculty.php
@@ -7,6 +7,7 @@
 			margin: 20;
 			padding: 0;
 			font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
 		}
 		nav {
 			position: fixed;
